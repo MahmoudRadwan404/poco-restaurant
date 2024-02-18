76d50a68c8504b540ab354615100be12831ce671
MahmoudRadwan404 <mahmoudrdwan32@gmail.com>
2024-02-18 14:40:20 +0200

    Quantity edit

--- src/controllers/cart/add-item.ts
+++ src/controllers/cart/add-item.ts
@@ -9,11 +9,19 @@ export default async function addItem(
 ) {
   const requestHandeler = handle(request);
   const itemId = requestHandeler.input("itemId");
-  const dishes=collection("dishes")
-  const dish=await dishes.findOne({_id:new ObjectId(itemId)})
-  const quantity=requestHandeler.input("quantity")
+  const quantity = +requestHandeler.input("quantity");
+  const dishes = collection("dishes");
+  const dish = await dishes.findOne({ _id: new ObjectId(itemId) });
+  if (quantity <= 0) {
+    return reply.send({ msg: "Quantity must be a positive number" });
+  }
   const userId = (request as any).user._id;
   const cartCollection = collection("cart");
-  await cartCollection.insertOne({ itemId, quantity, price:dish?.price, userId });
+  await cartCollection.insertOne({
+    itemId,
+    quantity,
+    price: dish?.price,
+    userId,
+  });
   return reply.code(201).send({ message: "Item added to the cart" });
 }
